f688e4327e5a34791ee5d9de6665460038d8e5e0
SananR <raosanan@gmail.com>
2025-03-16 01:06:16 -0400

    Finish section 6

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "pages#home"
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_08_223525) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_16_045339) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
